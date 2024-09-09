54cedb75106d02dedab1d1a6ab59080d1c4e96d9
os-global <sasha.susla@gmail.com>
2024-09-09 23:05:15 +0300

    Update debug settings and enable CORS in settings.py

--- autotestTarget/settings.py
+++ autotestTarget/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'completely unsafe key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_REPLACE_HTTPS_REFERER = True
 
 ALLOWED_HOSTS = ['*']
 
